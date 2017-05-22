9b67924c4b5d73b36891023902d59859e17e6aa3
Liam Ellis <liamhellis@gmail.com>
2017-05-22 15:25:40 -0400

    calendar and fuller home page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+
+
 jQuery(document).ready(function($){
 	var	scrolling = false;
 	var contentSections = $('.cd-section'),
@@ -54,4 +56,317 @@ jQuery(document).ready(function($){
         	300
         );
 	}
+	function calendar() {
+	    if ($.fn.fullCalendar && ($('#fitness-calendar').length || $('#yoga-calendar').length)) {
+	        var fitnessCalendar = $('#fitness-calendar').fullCalendar({
+	            minTime: '09:00:00',
+	            maxTime: '18:00:00',
+	            defaultView: 'agendaWeek',
+	            allDaySlot: false,
+	            columnFormat: 'dddd',
+	            slotDuration: '01:00:00',
+	            defaultDate: '2015-12-12',
+	            handleWindowResize: true,
+	            height: 'auto',
+	            defaultEventMinutes: 60,
+	            defaultTimedEventDuration: '01:00:00',
+	            header: {
+	                left: '',
+	                center: '',
+	                right: ''
+	            },
+	            hiddenDays: [0],
+	            events: [{
+	                title: 'Yoga Zen',
+	                start: '2015-12-07 09:00:00',
+	                className: 'indigo lighten-2'
+	            }, {
+	                title: 'Crosspin',
+	                start: '2015-12-07 10:00:00',
+	                className: 'orange accent-4'
+	            }, {
+	                title: 'Bike Spin',
+	                start: '2015-12-07 14:00:00',
+	                className: 'grey darken-4'
+	            }, {
+	                title: 'Zumba Dance',
+	                start: '2015-12-07 15:00:00',
+	                className: 'teal lighten-2'
+	            }, {
+	                title: 'Fight Cardio',
+	                start: '2015-12-07 16:00:00',
+	                className: 'indigo lighten-2'
+	            }, {
+	                title: 'WeighLifting',
+	                start: '2015-12-08 09:00:00',
+	                className: 'red lighten-1'
+	            }, {
+	                title: 'Crosspin',
+	                start: '2015-12-08 10:00:00',
+	                className: 'orange accent-4'
+	            }, {
+	                title: 'Zumba Dance',
+	                start: '2015-12-08 11:00:00',
+	                className: 'teal lighten-2'
+	            }, {
+	                title: 'WeighLifting',
+	                start: '2015-12-08 12:00:00',
+	                className: 'red lighten-1'
+	            }, {
+	                title: 'Yoga Zen',
+	                start: '2015-12-08 15:00:00',
+	                className: 'blue lighten-2'
+	            }, {
+	                title: 'Fight Cardio',
+	                start: '2015-12-08 17:00:00',
+	                className: 'indigo lighten-2'
+	            }, {
+	                title: 'Crosspin',
+	                start: '2015-12-09 10:00:00',
+	                className: 'orange accent-4'
+	            }, {
+	                title: 'Bike Spin',
+	                start: '2015-12-09 11:00:00',
+	                className: 'grey darken-4'
+	            }, {
+	                title: 'Zumba Dance',
+	                start: '2015-12-09 14:00:00',
+	                className: 'teal lighten-2'
+	            }, {
+	                title: 'Yoga Zen',
+	                start: '2015-12-09 15:00:00',
+	                className: 'blue lighten-2'
+	            }, {
+	                title: 'Fight Cardio',
+	                start: '2015-12-09 17:00:00',
+	                className: 'indigo lighten-2'
+	            }, {
+	                title: 'Bike Spin',
+	                start: '2015-12-10 09:00:00',
+	                className: 'grey darken-4'
+	            }, {
+	                title: 'Fight Cardio',
+	                start: '2015-12-10 10:00:00',
+	                className: 'indigo lighten-2'
+	            }, {
+	                title: 'Zumba Dance',
+	                start: '2015-12-10 11:00:00',
+	                className: 'teal lighten-2'
+	            }, {
+	                title: 'Crosspin',
+	                start: '2015-12-10 14:00:00',
+	                className: 'orange accent-4'
+	            }, {
+	                title: 'WeighLifting',
+	                start: '2015-12-10 16:00:00',
+	                className: 'red lighten-1'
+	            }, {
+	                title: 'Bike Spin',
+	                start: '2015-12-10 17:00:00',
+	                className: 'light-green darken-1'
+	            }, {
+	                title: 'Zumba Dance',
+	                start: '2015-12-11 09:00:00',
+	                className: 'teal lighten-2'
+	            }, {
+	                title: 'Fight Cardio',
+	                start: '2015-12-11 10:00:00',
+	                className: 'indigo lighten-2'
+	            }, {
+	                title: 'Yoga Zen',
+	                start: '2015-12-11 11:00:00',
+	                className: 'blue lighten-2'
+	            }, {
+	                title: 'Zumba Dance',
+	                start: '2015-12-11 14:00:00',
+	                className: 'teal lighten-2'
+	            }, {
+	                title: 'Bike Spin',
+	                start: '2015-12-11 16:00:00',
+	                className: 'light-green darken-1'
+	            }, {
+	                title: 'Yoga Zen',
+	                start: '2015-12-12 10:00:00',
+	                className: 'blue lighten-2'
+	            }, {
+	                title: 'Bike Spin',
+	                start: '2015-12-12 11:00:00',
+	                className: 'grey darken-4'
+	            }, {
+	                title: 'Crosspin',
+	                start: '2015-12-12 12:00:00',
+	                className: 'orange accent-4'
+	            }, {
+	                title: 'WeighLifting',
+	                start: '2015-12-12 15:00:00',
+	                className: 'red lighten-1'
+	            }, {
+	                title: 'Zumba Dance',
+	                start: '2015-12-12 16:00:00',
+	                className: 'teal lighten-2'
+	            }, {
+	                title: 'Yoga Zen',
+	                start: '2015-12-12 17:00:00',
+	                className: 'blue lighten-2'
+	            }],
+	            editable: false,
+	            droppable: false
+	        });
+	        var yogaCalendar = $('#yoga-calendar').fullCalendar({
+	            minTime: '09:00:00',
+	            maxTime: '18:00:00',
+	            defaultView: 'agendaWeek',
+	            allDaySlot: false,
+	            columnFormat: 'dddd',
+	            slotDuration: '01:00:00',
+	            defaultDate: '2015-12-12',
+	            handleWindowResize: true,
+	            height: 'auto',
+	            defaultEventMinutes: 60,
+	            defaultTimedEventDuration: '01:00:00',
+	            header: {
+	                left: '',
+	                center: '',
+	                right: ''
+	            },
+	            hiddenDays: [0],
+	            events: [{
+	                title: 'Yoga Zen',
+	                start: '2015-12-07 09:00:00',
+	                className: 'indigo lighten-2'
+	            }, {
+	                title: 'Prenatal Exercices',
+	                start: '2015-12-07 10:00:00',
+	                className: 'orange accent-4'
+	            }, {
+	                title: 'Vitality',
+	                start: '2015-12-07 14:00:00',
+	                className: 'grey darken-4'
+	            }, {
+	                title: 'Yummy',
+	                start: '2015-12-07 15:00:00',
+	                className: 'teal lighten-2'
+	            }, {
+	                title: 'Stretches',
+	                start: '2015-12-07 16:00:00',
+	                className: 'indigo lighten-2'
+	            }, {
+	                title: 'Rise & Shine',
+	                start: '2015-12-08 09:00:00',
+	                className: 'red lighten-1'
+	            }, {
+	                title: 'Prenatal Exercices',
+	                start: '2015-12-08 10:00:00',
+	                className: 'orange accent-4'
+	            }, {
+	                title: 'Yummy',
+	                start: '2015-12-08 11:00:00',
+	                className: 'teal lighten-2'
+	            }, {
+	                title: 'Rise & Shine',
+	                start: '2015-12-08 12:00:00',
+	                className: 'red lighten-1'
+	            }, {
+	                title: 'Yoga Zen',
+	                start: '2015-12-08 15:00:00',
+	                className: 'blue lighten-2'
+	            }, {
+	                title: 'Stretches',
+	                start: '2015-12-08 17:00:00',
+	                className: 'indigo lighten-2'
+	            }, {
+	                title: 'Prenatal Exercices',
+	                start: '2015-12-09 10:00:00',
+	                className: 'orange accent-4'
+	            }, {
+	                title: 'Vitality',
+	                start: '2015-12-09 11:00:00',
+	                className: 'grey darken-4'
+	            }, {
+	                title: 'Yummy',
+	                start: '2015-12-09 14:00:00',
+	                className: 'teal lighten-2'
+	            }, {
+	                title: 'Yoga Zen',
+	                start: '2015-12-09 15:00:00',
+	                className: 'blue lighten-2'
+	            }, {
+	                title: 'Stretches',
+	                start: '2015-12-09 17:00:00',
+	                className: 'indigo lighten-2'
+	            }, {
+	                title: 'Vitality',
+	                start: '2015-12-10 09:00:00',
+	                className: 'grey darken-4'
+	            }, {
+	                title: 'Stretches',
+	                start: '2015-12-10 10:00:00',
+	                className: 'indigo lighten-2'
+	            }, {
+	                title: 'Yummy',
+	                start: '2015-12-10 11:00:00',
+	                className: 'teal lighten-2'
+	            }, {
+	                title: 'Prenatal Exercices',
+	                start: '2015-12-10 14:00:00',
+	                className: 'orange accent-4'
+	            }, {
+	                title: 'Rise & Shine',
+	                start: '2015-12-10 16:00:00',
+	                className: 'red lighten-1'
+	            }, {
+	                title: 'Vitality',
+	                start: '2015-12-10 17:00:00',
+	                className: 'light-green darken-1'
+	            }, {
+	                title: 'Yummy',
+	                start: '2015-12-11 09:00:00',
+	                className: 'teal lighten-2'
+	            }, {
+	                title: 'Stretches',
+	                start: '2015-12-11 10:00:00',
+	                className: 'indigo lighten-2'
+	            }, {
+	                title: 'Yoga Zen',
+	                start: '2015-12-11 11:00:00',
+	                className: 'blue lighten-2'
+	            }, {
+	                title: 'Yummy',
+	                start: '2015-12-11 14:00:00',
+	                className: 'teal lighten-2'
+	            }, {
+	                title: 'Vitality',
+	                start: '2015-12-11 16:00:00',
+	                className: 'light-green darken-1'
+	            }, {
+	                title: 'Yoga Zen',
+	                start: '2015-12-12 10:00:00',
+	                className: 'blue lighten-2'
+	            }, {
+	                title: 'Vitality',
+	                start: '2015-12-12 11:00:00',
+	                className: 'grey darken-4'
+	            }, {
+	                title: 'Prenatal Exercices',
+	                start: '2015-12-12 12:00:00',
+	                className: 'orange accent-4'
+	            }, {
+	                title: 'Rise & Shine',
+	                start: '2015-12-12 15:00:00',
+	                className: 'red lighten-1'
+	            }, {
+	                title: 'Yummy',
+	                start: '2015-12-12 16:00:00',
+	                className: 'teal lighten-2'
+	            }, {
+	                title: 'Yoga Zen',
+	                start: '2015-12-12 17:00:00',
+	                className: 'blue lighten-2'
+	            }],
+	            editable: false,
+	            droppable: false
+	        });
+	    }
+	}
+	calendar();
 });
